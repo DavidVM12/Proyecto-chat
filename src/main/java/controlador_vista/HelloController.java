b639326c541d4bcfdde33889d89a51aee3b91527
David Velasco <david.santiago.velasc2@gmail.com>
2022-09-26 15:07:05 -0500

    Segundo borrador mas bonito

--- src/main/java/controlador_vista/HelloController.java
+++ src/main/java/controlador_vista/HelloController.java
@@ -2,13 +2,11 @@ package controlador_vista;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Hello");
-    }
+
 }

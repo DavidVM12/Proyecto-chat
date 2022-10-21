af4ee9937b8ff03af3f94cc5f76759a6c1fbc07a
David Velasco <david.santiago.velasc2@gmail.com>
2022-10-21 12:03:39 -0500

    interfaz chat: idea inicial

--- src/main/java/controlador_vista/Main.java
+++ src/main/java/controlador_vista/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("chat-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Char-Mander");
         Image image = new Image(getClass().getResourceAsStream("/imagenes/icono.png"));
